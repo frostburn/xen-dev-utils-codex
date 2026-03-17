ff1c3000e4d3d9d6cf318e0f11b9549c362641b3
Lumi Pakkanen <lumi.pakkanen@gmail.com>
2026-03-17 20:53:45 +0200

    lint: add eslint flat config for eslint v9 compatibility

---
+++ eslint.config.js
@@ -0,0 +1,36 @@
+const js = require('@eslint/js');
+const {FlatCompat} = require('@eslint/eslintrc');
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
+});
+
+const gtsConfigs = compat
+  .extends('./node_modules/gts/.eslintrc.json')
+  .map(config => ({...config, files: ['**/*.ts']}));
+
+module.exports = [
+  {
+    ignores: ['legacy/**', 'dist/**', 'docs/**'],
+  },
+  ...gtsConfigs,
+  {
+    files: ['**/*.ts'],
+    languageOptions: {
+      parserOptions: {
+        project: ['./tsconfig.eslint.json'],
+        tsconfigRootDir: __dirname,
+      },
+    },
+    rules: {
+      'no-empty': 0,
+      'no-constant-condition': 0,
+      '@typescript-eslint/no-explicit-any': 0,
+      '@typescript-eslint/ban-ts-comment': 0,
+      'prefer-const': ['error', {destructuring: 'all'}],
+      'no-restricted-syntax': ['error', 'SequenceExpression'],
+    },
+  },
+];
