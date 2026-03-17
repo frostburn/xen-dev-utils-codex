a034dd84a6c7fb251fc810cf59f5016b13e4bd14
Lumi Pakkanen <lumi.pakkanen@gmail.com>
2026-03-17 21:30:21 +0200

    lint: replace flat compat config with native eslint flat config

--- eslint.config.js
+++ eslint.config.js
@@ -1,32 +1,31 @@
-const js = require('@eslint/js');
-const {FlatCompat} = require('@eslint/eslintrc');
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-  recommendedConfig: js.configs.recommended,
-  allConfig: js.configs.all,
-});
+const tsParser = require('@typescript-eslint/parser');
+const tsPlugin = require('@typescript-eslint/eslint-plugin');
 
 module.exports = [
   {
     ignores: ['legacy/**', 'dist/**', 'docs/**', '**/*.js'],
   },
-  ...compat.extends('./node_modules/gts/'),
   {
     files: ['**/*.ts'],
     languageOptions: {
+      parser: tsParser,
       parserOptions: {
         project: ['./tsconfig.eslint.json'],
         tsconfigRootDir: __dirname,
+        sourceType: 'module',
+        ecmaVersion: 2020,
       },
+    },
+    plugins: {
+      '@typescript-eslint': tsPlugin,
     },
     rules: {
       'no-empty': 0,
       'no-constant-condition': 0,
-      '@typescript-eslint/no-explicit-any': 0,
-      '@typescript-eslint/ban-ts-comment': 0,
       'prefer-const': ['error', {destructuring: 'all'}],
       'no-restricted-syntax': ['error', 'SequenceExpression'],
+      '@typescript-eslint/no-explicit-any': 0,
+      '@typescript-eslint/ban-ts-comment': 0,
     },
   },
 ];
